98dd9b56cc913b82e4db2e800fa28df6b144f269
dpg7085 <dpg7085@g.rit.edu>
2020-11-02 09:34:03 -0500

    Wrote blamer for Archeogit

--- archeogitvsszz/blamers/archeogit.py
+++ archeogitvsszz/blamers/archeogit.py
@@ -1,4 +1,6 @@
 import logging
+import subprocess
+import csv
 
 from . import base
 
@@ -7,4 +9,13 @@ logger = logging.getLogger(__name__)
 
 class Archeogit(base.BaseBlamer):
     def blame(self, sha):
-        pass
+        result = subprocess.run(['archeogit', 'blame', '--csv', str(self._repository.path), sha],
+                                stdout=subprocess.PIPE)
+
+        contributors = []
+        for line in csv.DictReader(result):
+            contributors.append(line['contributor'])
+        print("Contributors: ")
+        print(contributors)
+
+        return contributors
